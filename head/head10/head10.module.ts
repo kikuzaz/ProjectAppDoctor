afc1d11bda30c6aa4f11ecc004449aacc106303e
dowSirilak <s5802041630055@email.kmutnb.ac.th>
2018-11-06 12:44:46 +0700

    add file to description branch

---
+++ head/head10/head10.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Head10Page } from './head10';
+
+@NgModule({
+  declarations: [
+    Head10Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Head10Page),
+  ],
+})
+export class Head10PageModule {}
---
+++ head/head18/head18.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Head18Page } from './head18';
+
+@NgModule({
+  declarations: [
+    Head18Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Head18Page),
+  ],
+})
+export class Head18PageModule {}
