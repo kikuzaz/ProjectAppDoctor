52e45022170ae2cfb941f91644080ec86e294576
niphaphorn <s5802041620114@email.kmutnb.ac.th>
2018-11-12 22:58:31 -0800

    add file login

---
+++ Login/src/pages/login/login.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { RegisterPage } from '../register/register';
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+
+  GoToRegister(){
+    this.navCtrl.push(RegisterPage)
+  }
+
+}
