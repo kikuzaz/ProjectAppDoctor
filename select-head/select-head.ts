38fceaa0d96e53740e9570fca6f28b59851d1936
Waternum <S5802041630039@email.kmutnb.ac.th>
2018-12-11 04:03:28 +0700

    Select-headPage

---
+++ select-head/select-head.ts
@@ -0,0 +1,151 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { Head1Page } from '../head1/head1';
+import { Head2Page } from '../head2/head2';
+import { Head3Page } from '../head3/head3';
+import { Head4Page } from '../head4/head4';
+import { Head5Page } from '../head5/head5';
+import { Head6Page } from '../head6/head6';
+import { Head7Page } from '../head7/head7';
+import { Head8Page } from '../head8/head8';
+import { Head9Page } from '../head9/head9';
+import { Head10Page } from '../head10/head10';
+import { Head11Page } from '../head11/head11';
+import { Head12Page } from '../head12/head12';
+import { Head13Page } from '../head13/head13';
+import { Head14Page } from '../head14/head14';
+import { Head15Page } from '../head15/head15';
+import { Head16Page } from '../head16/head16';
+import { Head17Page } from '../head17/head17';
+import { Head18Page } from '../head18/head18';
+import { Head19Page } from '../head19/head19';
+import { Head20Page } from '../head20/head20';
+import { Head21Page } from '../head21/head21';
+import { Head22Page } from '../head22/head22';
+import { Head23Page } from '../head23/head23';
+import { Head24Page } from '../head24/head24';
+import { Head25Page } from '../head25/head25';
+
+/**
+ * Generated class for the SelectHeadPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-select-head',
+  templateUrl: 'select-head.html',
+})
+export class SelectHeadPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SelectHeadPage');
+  }
+
+  gotoHead1(){
+    this.navCtrl.push(Head1Page);
+  }
+
+  gotoHead2(){
+    this.navCtrl.push(Head2Page);
+  }
+
+  gotoHead3(){
+    this.navCtrl.push(Head3Page);
+  }
+
+  gotoHead4(){
+    this.navCtrl.push(Head4Page);
+  }
+
+  gotoHead5(){
+    this.navCtrl.push(Head5Page);
+  }
+
+  gotoHead6(){
+    this.navCtrl.push(Head6Page);
+  }
+
+  gotoHead7(){
+    this.navCtrl.push(Head7Page);
+  }
+
+  gotoHead8(){
+    this.navCtrl.push(Head8Page);
+  }
+
+  gotoHead9(){
+    this.navCtrl.push(Head9Page);
+  }
+
+  gotoHead10(){
+    this.navCtrl.push(Head10Page);
+  }
+
+  gotoHead11(){
+    this.navCtrl.push(Head11Page);
+  }
+
+  gotoHead12(){
+    this.navCtrl.push(Head12Page);
+  }
+
+  gotoHead13(){
+    this.navCtrl.push(Head13Page);
+  }
+
+  gotoHead14(){
+    this.navCtrl.push(Head14Page);
+  }
+
+  gotoHead15(){
+    this.navCtrl.push(Head15Page);
+  }
+
+  gotoHead16(){
+    this.navCtrl.push(Head16Page);
+  }
+
+  gotoHead17(){
+    this.navCtrl.push(Head17Page);
+  }
+
+  gotoHead18(){
+    this.navCtrl.push(Head18Page);
+  }
+
+  gotoHead19(){
+    this.navCtrl.push(Head19Page);
+  }
+
+  gotoHead20(){
+    this.navCtrl.push(Head20Page);
+  }
+
+  gotoHead21(){
+    this.navCtrl.push(Head21Page);
+  }
+
+  gotoHead22(){
+    this.navCtrl.push(Head22Page);
+  }
+
+  gotoHead23(){
+    this.navCtrl.push(Head23Page);
+  }
+
+  gotoHead24(){
+    this.navCtrl.push(Head24Page);
+  }
+
+  gotoHead25(){
+    this.navCtrl.push(Head25Page);
+  }
+
+}
