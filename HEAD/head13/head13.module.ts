4c84b8e735e4fa77287d6d01009f2ddefabc8d89
dowSirilak <s5802041630055@email.kmutnb.ac.th>
2018-12-11 00:59:30 +0700

    add folder head to branch information

---
+++ HEAD/head13/head13.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Head13Page } from './head13';
+
+@NgModule({
+  declarations: [
+    Head13Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Head13Page),
+  ],
+})
+export class Head13PageModule {}
---
+++ HEAD/head14/head14.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Head14Page } from './head14';
+
+@NgModule({
+  declarations: [
+    Head14Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Head14Page),
+  ],
+})
+export class Head14PageModule {}
