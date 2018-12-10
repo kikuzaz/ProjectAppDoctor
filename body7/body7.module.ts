6420d7f4f1ebc39ee88fae5bbbb87abb702a72df
Waternum <S5802041630039@email.kmutnb.ac.th>
2018-12-11 04:10:21 +0700

    infoBodyPage

---
+++ body7/body7.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Body7Page } from './body7';
+
+@NgModule({
+  declarations: [
+    Body7Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Body7Page),
+  ],
+})
+export class Body7PageModule {}
---
+++ body9/body9.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Body9Page } from './body9';
+
+@NgModule({
+  declarations: [
+    Body9Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Body9Page),
+  ],
+})
+export class Body9PageModule {}
