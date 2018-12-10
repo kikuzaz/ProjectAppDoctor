1f7ccfbe5fa0dfa09c29df6fb4b99de921f5028a
kikuzaz <s5802041620092@email.kmutnb.ac.th>
2018-12-10 23:02:57 +0700

    Restart

--- hospital1/src/app/shared.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SuperTabsModule } from 'ionic2-super-tabs';
-
-@NgModule({
-  exports: [
-    SuperTabsModule
-  ]
-})
-export class SharedModule {}
