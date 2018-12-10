79156aec6a5a39b84ff4cbb76cda434c4beb2346
Waternum <S5802041630039@email.kmutnb.ac.th>
2018-12-11 04:12:22 +0700

    infoBottomPage

---
+++ bottom6/bottom6.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Bottom6Page } from './bottom6';
+
+@NgModule({
+  declarations: [
+    Bottom6Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Bottom6Page),
+  ],
+})
+export class Bottom6PageModule {}
